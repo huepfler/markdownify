f3a1f456e407d5744a33fc223e4d631bef6b88fd
sp4ce <sp4ce@pop-os.localdomain>
2020-04-06 16:31:50 -0400

    adding copy buttons to the editor

--- src/main/markdownify/main.cljs
+++ src/main/markdownify/main.cljs
@@ -11,6 +11,23 @@
 (defn md->html [md]
   (.makeHtml showdown-converter md))
 
+(defn copy-to-clipboard [s]
+  (let [el (.createElement js/document "textarea")
+        selected (when (pos? (-> js/document .getSelection .-rangeCount))
+                   (-> js/document .getSelection (.getRangeAt 0)))]
+    (set! (.-value el) s)
+    (.setAttribute el "readonly" "")
+    (set! (-> el .-style .-position) "absolute")
+    (set! (-> el .-style .-left) "-9999px")
+    (-> js/document .-body (.appendChild el))
+    (.select el)
+    (.execCommand js/document "copy")
+    (-> js/document .-body (.removeChild el))
+    (when selected
+      (-> js/document .getSelection .removeAllRanges)
+      (-> js/document .getSelection (.addRange selected)))))
+
+
 (defn app []
   [:div
    [:h1 "Hi, there!"]
@@ -23,15 +40,29 @@
       {:on-change #(reset! markdown (-> % .-target .-value))
        :value @markdown
        :style {:resize "none"
-               :height "500px"
-               :width "100%"}}]]
+               :height "350px"
+               :width "100%"}}]
+     [:button
+      {:on-click #(copy-to-clipboard @markdown)
+       :style {:background-color :green
+               :padding "1em"
+               :color :white
+               :border-radius 10}}
+      "Copy Markdown"]]
 
     [:div
      {:style {:flex "1"
               :padding-left "2em"}}
      [:h2 "HTML Preview"]
-     [:div {:style {:height "500px"}
-            :dangerouslySetInnerHTML {:__html (md->html @markdown)}}]]]])
+     [:div {:style {:height "350px"}
+            :dangerouslySetInnerHTML {:__html (md->html @markdown)}}]
+     [:button
+      {:on-click #(copy-to-clipboard (md->html @markdown))
+       :style {:background-color :green
+               :padding "1em"
+               :color :white
+               :border-radius 10}}
+      "Copy HTML"]]]])
 
 
 ;;
